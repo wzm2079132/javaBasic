c2b5dbe04f9c9cae8dfb2baab582ec3e1b8b51ae
wzm2079132 <2079132@qq.com>
2019-05-23 20:05:57 +0800

    test

--- src/com/neuedu/test/HomeWork0523.java
+++ src/com/neuedu/test/HomeWork0523.java
@@ -2,16 +2,13 @@ package com.neuedu.test;
 
 public class HomeWork0523 {
     public static void main(String[] args) {
-        int num=1234;
-        int result=0;
-        int s=0;
-        int i=1000;
+        int num =12345;
+        int result =0;
+        int s =0;
         do {
             s = num % 10;
             num /= 10;
-            s*=i;
-            i /=10;
-            result +=s;
+            result = result*10 + s;
         }while (num!=0);
 
         System.out.println(result);
