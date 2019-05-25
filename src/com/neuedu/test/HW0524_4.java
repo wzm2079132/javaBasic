c230d380e24bbeadbcba49b3b01cb762e33b23c4
wzm2079132 <2079132@qq.com>
2019-05-25 15:02:02 +0800

    test

--- src/com/neuedu/test/HW0524_4.java
+++ src/com/neuedu/test/HW0524_4.java
@@ -8,7 +8,7 @@ public class HW0524_4 {
         Scanner input = new Scanner(System.in);
         int a = input.nextInt();
         int result = 1;
-        for (a=a+1;a>=1;a--){
+        for (;a>=1;a--){
             result *= a ;
         }
         System.out.println(result);
