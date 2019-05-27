feb3ab6e6a7867b02ec2981f818db73e2564bd22
wzm2079132 <2079132@qq.com>
2019-05-27 22:17:31 +0800

    test

--- src/com/neuedu/test/StuTest.java
+++ src/com/neuedu/test/StuTest.java
@@ -2,57 +2,22 @@ package com.neuedu.test;
 
 public class StuTest {
     public static void main(String[] args) {
-        Student s1=new Student();
-        s1.name="王大";
-        s1.age=7;
-        s1.score=74;
+        Student s1=new Student("王大",8,95.0);
+        Student s2=new Student("赵二",9,72.5);
+        Student s3=new Student("张三",7,88.5);
+        Student s4=new Student("李四",8,61.5);
+        Student s5=new Student("钱五",6,85.0);
 
-        Student s2=new Student();
-        s2.name="赵二";
-        s2.age=8;
-        s2.score=89;
+        Student[] stu=new Student[5];
+        stu[0]=s1;
+        stu[1]=s2;
+        stu[2]=s3;
+        stu[3]=s4;
+        stu[4]=s5;
+        Student.sort(stu);
 
-        Student s3=new Student();
-        s3.name="张三";
-        s3.age=9;
-        s3.score=66;
-
-        Student s4=new Student();
-        s4.name="李四";
-        s4.age=6;
-        s4.score=56;
-
-        Student s5=new Student();
-        s5.name="钱五";
-        s5.age=7;
-        s5.score=96;
-
-        String n1=s1.name+s1.age+s1.score;
-        String n2=s2.name+s2.age+s2.score;
-        String n3=s3.name+s3.age+s3.score;
-        String n4=s4.name+s4.age+s4.score;
-        String n5=s5.name+s5.age+s5.score;
-
-        int[] sco={s1.score,s2.score,s3.score,s4.score,s5.score};
-        String[] n={n1,n2,n3,n4,n5};
-            for (int i=0;i<sco.length;i++){
-                for (int j=0;j<sco.length-1-i;j++){
-                    int t=0;
-                    String m="null";
-                    if (sco[j]<sco[j+1]){
-                        t=sco[j];
-                        sco[j]=sco[j+1];
-                        sco[j+1]=t;
-
-                        m=n[j];
-                        n[j]=n[j+1];
-                        n[j+1]=m;
-                    }
-                }
-            }
-
-        System.out.println(n);
-        }
     }
+
+}
 
 
--- src/com/neuedu/test/Student.java
+++ src/com/neuedu/test/Student.java
@@ -4,11 +4,49 @@ public class Student {
 
     String name;
     int age;
-    int score;
+    double score;
 
-    public void rank(){
-        for (int i=1;i<=5;i++){
+
+    public double getScore() {
+        return score;
+    }
+
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+
+    public Student(String name, int age, double score) {
+        super();
+        this.name = name;
+        this.age = age;
+        this.score = score;
+    }
+
+
+    public String toString() {
+        return "学生 [姓名=" + name + ", 年龄=" + age
+                + ", 成绩=" + score + "]";
+    }
+
+
+    public static void sort(Student[] stu) {
+        for (int i = 0; i < stu.length; i++) {
+            for (int j = i; j < stu.length; j++) {
+                if (stu[i].getScore() < stu[j].getScore()) {
+                    Student tem = stu[j];
+                    stu[j] = stu[i];
+                    stu[i] = tem;
+                }
+            }
 
         }
+        System.out.println("成绩第一" + stu[0].toString());
+        System.out.println("成绩第二" + stu[1].toString());
+        System.out.println("成绩第三" + stu[2].toString());
+        System.out.println("成绩第四" + stu[3].toString());
+        System.out.println("成绩第五" + stu[4].toString());
     }
+
 }
